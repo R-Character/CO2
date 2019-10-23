04927f4c77620ca83003a964b1c7f2252585c1a3
Ramin Farazandeh <ramin.farazandeh@outlook.com>
2019-10-23 20:40:26 +0200

    Create new Branch

--- CO2/Form1.Designer.cs
+++ CO2/Form1.Designer.cs
@@ -286,7 +286,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(262, 187);
+            this.label6.Location = new System.Drawing.Point(261, 187);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(65, 20);
             this.label6.TabIndex = 18;
@@ -296,7 +296,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(135, 187);
+            this.label5.Location = new System.Drawing.Point(136, 187);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(59, 20);
             this.label5.TabIndex = 18;
