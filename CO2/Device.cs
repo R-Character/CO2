9b15ce1b4c5aab00fe85af993af113fba5581056
sheykhi <53047085+sheykhi@users.noreply.github.com>
2019-10-31 18:26:17 +0100

    Kommentare hinzugefügt

--- CO2/Device.cs
+++ CO2/Device.cs
@@ -2,25 +2,26 @@
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client;  //importieren von Azure Bibliotheken
 
 namespace CO2
 {
     public class Device
     {
+        //Klassenvariablen
         private string myConString;
         private DeviceClient myDev;
 
 
 
 
-        public Device ()
+        public Device () //Konstruktor ohne Connection String
         {
             ResetConString();
             myDev = DeviceClient.CreateFromConnectionString(myConString);
         }
 
-        public Device (string conString)
+        public Device (string conString) //Konstruktor mit Connection String als Rückgabewert
         {
             SetConString(conString);
             try
@@ -35,34 +36,34 @@ namespace CO2
 
         }
 
-        public void ResetConString()
+        public void ResetConString() //Connection String wird initialisiert
         {
             myConString = "HostName=CO2-SensorData-Dukes.azure-devices.net;DeviceId=CO2SimulatorDukes;SharedAccessKey=jtBS7Ef1Msuj01bsisaovhknHPNy7Q48XPHMbj+OTzM=";
         }
         
-        public void SetConString(string conString)
+        public void SetConString(string conString)//Setter-Methode des Connection String
         {
             myConString = conString;
         }
 
-        public string GetConString()
+        public string GetConString() //Getter-Methode des Connections String
         {
             return myConString;            
         }
 
-        public string getDeviceName()
+        public string getDeviceName() //Getter-Methode des Device Name
         {
             IotHubConnectionStringBuilder myDeviceID = IotHubConnectionStringBuilder.Create(myConString);
-            return myDeviceID.DeviceId;
+            return myDeviceID.DeviceId; //für DeviceID braucht man ein Objekt von der Klasse "IoTHubConnectionStringBuilder"
         }
 
-        public void SendMessageToAzureIoTHub(string message)
+        public void SendMessageToAzureIoTHub(string message)// Nachricht wird an IoTHub zurückgegeben
         {
             if (myDev != null)
             {
                 try
                 {
-                    var myMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(message));
+                    var myMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(message)); //ein Objekt für Message wird deklariert
                     myDev.SendEventAsync(myMessage); // send message
                 }
                 catch (Exception ex)
@@ -72,23 +73,23 @@ namespace CO2
             }
         }
 
-        public void RecievMessageFromAzureIoTHub()
+        public void RecievMessageFromAzureIoTHub()//Empfangen der Nachricht von IoTHub
         {
-            if (myDev != null)
+            if (myDev != null) //If-Anweisung wird ausgelöst, sobald ein Device bereit zu initialisieren ist
             {
-                Task.Run(async () =>
+                Task.Run(async () =>   //asynchrone Ausführung
                 {
                     while (true)
                     {
                         Microsoft.Azure.Devices.Client.Message myReceivedMessage = null;
                         try
                         {
-                            myReceivedMessage = await myDev.ReceiveAsync();
+                            myReceivedMessage = await myDev.ReceiveAsync(); //Warten auf empfangen einer Nachricht
                             if (myReceivedMessage != null)
                             {
-                                byte[] data = myReceivedMessage.GetBytes();
-                                MessageBox.Show(Encoding.UTF8.GetString(data, 0, data.Length) + "\r\n", "Eine neue Nachricht", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                await myDev.CompleteAsync(myReceivedMessage);
+                                byte[] data = myReceivedMessage.GetBytes();  //Bytes werden in einem Array gespeichert
+                                MessageBox.Show(Encoding.UTF8.GetString(data, 0, data.Length) + "\r\n", "Eine neue Nachricht", MessageBoxButtons.OK, MessageBoxIcon.Information); //Konvertieren zu einem String
+                                await myDev.CompleteAsync(myReceivedMessage); //asynchrone Ausführung wird beendet
                             }
                         }
                         catch (Exception ex)
--- CO2/Form1.cs
+++ CO2/Form1.cs
@@ -9,81 +9,84 @@ namespace CO2
         Device myDevice = new Device();
         public int period;
         public bool myFlag = true;
-        int CO2Wert = 0;
+        int CO2Wert = 0; //Anfangswert
 
         public Form1()
         {
             InitializeComponent();
             label10.Hide();
-            txtboxEinstellungConnectionString.Text = myDevice.GetConString();
-            period = Convert.ToInt32(value: comboBox1.Text);
+            txtboxEinstellungConnectionString.Text = myDevice.GetConString(); //Connection String wird in Textbox geschrieben
+            period = Convert.ToInt32(value: comboBox1.Text); //Periode wird eingestellt
 
         }
 
-        private void BtnSend_Click(object sender, EventArgs e)
+        private void BtnSend_Click(object sender, EventArgs e) //Button Start oder Stop
         {
-            if (myFlag)
+            if (myFlag)             //stoppen des Sendevorgangs
             {
                 RaumGroesse.Enabled = trkInitialWert.Enabled = myFlag =
-                    tabEinstellung.Enabled = InitialWert.Enabled = false;
+                    tabEinstellung.Enabled = InitialWert.Enabled = false; 
                 btnSend.Text = "Stop";
 
-                label10.Show();
+                label10.Show(); //"Bereit Nachricht zu empfangen" wird angezeigt
                 this.timer1.Start();
             }
             else
-            {
+            {           //Start des Sendevorgangs
                 RaumGroesse.Enabled = trkInitialWert.Enabled = myFlag =
                     tabEinstellung.Enabled = InitialWert.Enabled = true;
                 btnSend.Text = "Start";
-                label10.Hide();
+                label10.Hide(); //"Bereit Nachricht zu empfangen" wird nicht angezeigt
                 this.timer1.Stop();
             }
         }
 
-
-        private void BtnEinstellungDefault_Click(object sender, EventArgs e)
+        //Connection String wird auf Default zurückgestellt
+        private void BtnEinstellungDefault_Click(object sender, EventArgs e)  //Button "Deafault" in Reiter Einstellung
         {
-            myDevice.ResetConString();
+            myDevice.ResetConString();                                                  
             txtboxEinstellungConnectionString.Text = myDevice.GetConString();
         }
-
-        private void TrkInitialWert_Scroll(object sender, EventArgs e)
+        //Trackbar
+        private void TrkInitialWert_Scroll(object sender, EventArgs e) //Scroll Balken für den inital Wert
         {
             InitialWert.Text = "" + trkInitialWert.Value;
         }
 
-        private void BtnEinstellungAktualisieren_Click(object sender, EventArgs e)
+        private void BtnEinstellungAktualisieren_Click(object sender, EventArgs e)//Button für "Einstellungen aktualisieren"
         {
             period = Convert.ToInt32(value: comboBox1.Text); // update Periode
-            myDevice = new Device(txtboxEinstellungConnectionString.Text);
-            MessageBox.Show("Die Einstellungen werden aktualisiert.", "Aktualisiert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //alter Conncection String wird für aktulles Objekt aktualisiert
+            myDevice = new Device(txtboxEinstellungConnectionString.Text); //neuer Connection String wird übernommen
+            MessageBox.Show("Die Einstellungen werden aktualisiert.", "Aktualisiert", MessageBoxButtons.OK, MessageBoxIcon.Information); 
         }
 
-        private void Timer1_Tick(object sender, EventArgs e)
+        private void Timer1_Tick(object sender, EventArgs e) //Timer einstellen
         {
-            DateTime now = DateTime.Now;
+            DateTime now = DateTime.Now; 
             if (progressBar1.Value != 100)
                 this.progressBar1.Increment(50 / period);
             else
             {
-                string msg = Convert.ToString(CalcCO2()) + "ppm CO2 pro Stunde \tum " + now.ToString("HH:mm:ss") + "\tvom Gerät: " + myDevice.getDeviceName();
-                myDevice.SendMessageToAzureIoTHub(msg);
-                myDevice.RecievMessageFromAzureIoTHub();
+                //Erstellt eine Nachricht und bereit Nachrichten von IoTHub zu empfangen sowie zu senden
+                string msg = Convert.ToString(CalcCO2()) + "ppm CO2 pro Stunde \tum " + now.ToString("HH:mm:ss") + "\tvom Gerät: " + myDevice.getDeviceName(); //angezeigte Nachricht(Data)
+                myDevice.SendMessageToAzureIoTHub(msg);  //senden von Nachricht an IoTHub
+                myDevice.RecievMessageFromAzureIoTHub(); // empfangen von Message an IoTHub
                 progressBar1.Value = 0;
             }
         }
 
-        private void TxtInitialWert_Leave(object sender, EventArgs e)
+        private void TxtInitialWert_Leave(object sender, EventArgs e) //initalWert Eingabe
         {
             int myWert = Convert.ToInt32(InitialWert.Text);
-            if (myWert >= 800 && myWert <= 1000)
+            if (myWert >= 800 && myWert <= 1000)        //Prüfung, ob der eingegebene inital Wert im Wertebereich liegt
                 trkInitialWert.Value = Convert.ToInt32(InitialWert.Text);
-            else
+            else //für den Fall, das der inital Wert nicht im Wertebereich liegt
                 MessageBox.Show("Der CO2 Initialwert muss zwischen 800 und 1000 sein!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
 
-        private int CalcCO2()
+        private int CalcCO2() //Berechnung des Co2 Wert
         {
             CO2Wert = (Convert.ToInt32(RaumGroesse.Text)) * (Convert.ToInt32(InitialWert.Text));
             CO2Wert += (250 * Convert.ToInt32(Personen.Text)) - (10 * Convert.ToInt32(Fenster.Text));
